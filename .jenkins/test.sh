054050588ed56022cd950e26d311249673e15765
echung <echung@uber.com>
2016-11-02 17:21:14 -0700

    Fix test.sh so it works in jenkins
    Test Plan: jenkins
    
    Reviewers: akhera
    
    Reviewed By: akhera
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D620504

--- .jenkins/test.sh
+++ .jenkins/test.sh
@@ -1,1 +1,20 @@
-export GOPATH=${WORKSPACE}/gocode && make test
+#!/bin/bash
+
+# Setup constants
+export GOPATH=${WORKSPACE}/.jenkins-tmp
+REPODIR="${GOPATH}/src/code.uber.internal/infra/mesos-go"
+
+# Create go workspace
+mkdir -p ${REPODIR} &&
+
+# rsync everything to go workspace
+rsync -avzu ${WORKSPACE}/ ${REPODIR}/ --exclude=${REPODIR} --exclude=vendor --exclude=.git &&
+
+# cd into workspace
+cd ${REPODIR} &&
+
+# Install dependencies with glide
+glide install &&
+
+# Run test
+go test
