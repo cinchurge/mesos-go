24fe39852a992b25d55b304cfd3f65f4bd4cdbe6
echung <echung@uber.com>
2016-11-04 01:29:01 +0000

    fix test and coverage reports
    Test Plan: jenkins
    
    Reviewers: akhera
    
    Reviewed By: akhera
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D622658

--- .jenkins/test.sh
+++ .jenkins/test.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-# Setup constants
+set -vex
+
+# Setup GOPATH and repodir
 export GOPATH=${WORKSPACE}/.jenkins-tmp
+export PATH=${GOPATH}/bin:${PATH}
 REPODIR="${GOPATH}/src/code.uber.internal/infra/mesos-go"
 
 # Create go workspace
-mkdir -p ${REPODIR} &&
+mkdir -p ${REPODIR}
 
 # rsync everything to go workspace
-rsync -avzu ${WORKSPACE}/ ${REPODIR}/ --exclude=${REPODIR} --exclude=vendor --exclude=.git &&
+rsync -avzu ${WORKSPACE}/ ${REPODIR}/ --exclude=${REPODIR} --exclude=vendor --exclude=.git
 
 # cd into workspace
-cd ${REPODIR} &&
+cd ${REPODIR}
+
+# Install test dependencies
+go get -u github.com/jstemmer/go-junit-report
+go get -u github.com/axw/gocov/
+go get -u github.com/AlekSi/gocov-xml
 
 # Install dependencies with glide
-glide install &&
+glide install
 
 # Run test
-go test
+go test -v -coverprofile=coverage.out | tee test.log
+
+# Generate test report
+go-junit-report < test.log > ${WORKSPACE}/junit.xml
+
+# Generate coverage report
+go tool cover -html=coverage.out
+gocov convert coverage.out | gocov-xml | sed 's|filename=".*code.uber.internal/infra/mesos-go/|filename="|' > ${WORKSPACE}/coverage.xml
